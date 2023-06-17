c9827df9c3e2e0f661c3fa8d279a985ced8db3c7
jiwannishad <jiwan@leadangel.com>
2023-06-17 12:30:25 +0530

    dialog change

--- src/app/admin/transaction/transaction.module.ts
+++ src/app/admin/transaction/transaction.module.ts
@@ -8,24 +8,61 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ComponentsModule } from 'src/app/shared/components/components.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { DatatableModule } from '../common/datatable.module';
+import { AddeditComponent } from './dialogs/addedit/addedit.component';
+import { DeleteComponent } from './dialogs/delete/delete.component';
 
 
 
 @NgModule({
   declarations: [
-    InscanComponent
+    InscanComponent,
+    AddeditComponent,
+    DeleteComponent
+
   ],
   imports: [
     CommonModule,
     TransactionRoutingModule,
     FormsModule,
-    MatButtonModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
     MatFormFieldModule,
     MatInputModule,
-    ReactiveFormsModule,
-    MatIconModule
+    MatSnackBarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSortModule,
+    MatTooltipModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatTabsModule,
+    MatTooltipModule,
+    ComponentsModule,
+    SharedModule,
+    DatatableModule,
+    MatAutocompleteModule
   ],
-  providers:[TransactionService]
+  providers:[TransactionService],
+  exports:[AddeditComponent,DeleteComponent]
 
 })
 export class TransactionModule { }
