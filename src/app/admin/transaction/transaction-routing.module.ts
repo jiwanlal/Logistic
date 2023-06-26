0140a53a8ca1fe7ffdb9e4dd82a51193207718e8
jiwannishad <jiwan@leadangel.com>
2023-06-26 21:49:06 +0530

    dashboard and delivery page change

--- src/app/admin/transaction/transaction-routing.module.ts
+++ src/app/admin/transaction/transaction-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InscanComponent } from './inscan/inscan.component';
 import { DrsofdComponent } from './drsofd/drsofd.component';
+import { DeliveryComponent } from './delivery/delivery.component';
 
 const routes: Routes = [ 
   {path:"inscan", component:InscanComponent},
-  {path:"drsofd", component:DrsofdComponent}
+  {path:"drsofd", component:DrsofdComponent},
+  {path:"delivery", component:DeliveryComponent},
+  
 ];
 
 @NgModule({
